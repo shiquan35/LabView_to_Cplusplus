1aaf6a9ef91a33a43122066267df516172a4b08c
shiquan35 <86793931+shiquan35@users.noreply.github.com>
2021-09-30 14:38:05 +0800

    Add files via upload
    setup connection and interface

--- main.cpp
+++ main.cpp
@@ -15,55 +15,28 @@
 #include <supportLib.hpp>
 #define N 16
 
-
-/*
-// FFT function class - vector of complex numbers of type DOUBLE, function called "dft",
-// accepting variable 'X' which is a vector of complex numbers
-std::vector<std::complex<double>> dft(std::vector<std::complex<double>> X)
-{
-	// Determine number of samples
-	int N = X.size();
-	int K = N;
-
-	// Allocate memory for internals
-	std::complex<double> intSum;
-
-	// Allocate memory for output
-	std::vector<std::complex<double>> output;
-	output.reserve(K);
-
-	// Loop through each K
-	for (int k = 0; k < K; k++)
-	{
-		// Loop through each n
-		intSum = std::complex<double>(0, 0);
-		for (int n = 0; n < N; n++)
-		{
-			double realPart = cos(((2 * M_PI) / N) * k * n);
-			double imagPart = sin(((2 * M_PI) / N) * k * n);
-			std::complex<double> w(realPart, -imagPart);
-			intSum += X[n] * w;
-		}
-		output.push_back(intSum);
-	}
-	return output;
-}
+/* IMAQ runs in x86
+   fftw runs in x64(?)
 */
 
 
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* REOPEN LINE 49
 int main()
 {
+	
 	// structure to hold the image
 	RGBABitmapImageReference* imageRef = CreateRGBABitmapImageReference();
 	RGBABitmapImageReference* imageRef2 = CreateRGBABitmapImageReference();
-	//* imageRef3 = CreateRGBABitmapImageReference();
+	// imageRef3 = CreateRGBABitmapImageReference();
 	std::vector <double> a; // x coordinate of FFT
 	std::vector <double> b; // y coordinate of FFT
 	std::vector <double> c; // x coordinate of signal
 	std::vector <double> d; // y coordinate of signal
+	
 
-	/*
+
+	
 	// CORRECT COSINE WAVE 16 SEPT
 	//std::cout << 'x' << "\t";
 	//std::cout << 'y' << "\t" << std::endl;
@@ -76,10 +49,8 @@ int main()
 		//std::cout << c[f] << "\t";
 		//std::cout << d[f] << std::endl;
 	}
-	
-	 
-	
-
+	*/
+	/*
 	std::cout << "Cosine complete" << std::endl;
 	DrawScatterPlot(imageRef, 600, 400, &c, &d); // width, height
 
@@ -88,22 +59,24 @@ int main()
 	WriteToFile(pngdata, "cosine3.png");
 	std::cout << "Plot Created" << std::endl;
 	DeleteImage(imageRef->image);
-	
 	*/
 
-
+	/* REOPEN LINE 71
 	// beginning of FFTW
 	fftw_complex in[N], out[N], in2[N];
 	fftw_plan p, q;
 	int i;
 
-	/* prepare a cosine wave */
+	// prepare a cosine wave
 	for (i = 0; i < N; i++) {
 		in[i][0] = cos(3 * 2 * M_PI * i / N);
 		in[i][1] = 0;
 	}
+	*/
 
-	/* plot cosine
+
+	/*
+	// plot cosine
 	for (int j = 0; j < 15; ++j)
 	{
 		c.push_back(j); // c: 0-360
@@ -119,8 +92,8 @@ int main()
 	DeleteImage(imageRef2->image);
 	*/
 
-
-	/* forward Fourier transform, save the result in 'out' */
+	/* REOPEN LINE 145
+	// forward Fourier transform, save the result in 'out'
 	p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
 	fftw_execute(p);
 	for (i = 0; i < N; i++)
@@ -145,29 +118,20 @@ int main()
 		
 	}
 	std::cout << "test end" << std::endl;
-	/*
-	DrawScatterPlot(imageRef, 600, 400, &a, &b); // width, height
 
-	//convert plot to PNG file
-	vector<double>* pngdata = ConvertToPNG(imageRef->image);
-	WriteToFile(pngdata, "FFTcosine.png");
-	std::cout << "Plot Created" << std::endl;
-	DeleteImage(imageRef->image);
-	*/
-
-
-	//convert plot to PNG file BARPLOT
+	//convert FFT plot to PNG file BARPLOT
 	imageRef->image = DrawBarPlot(800.0, 600.0, &b);
 	vector<double>* pngdata = ConvertToPNG(imageRef->image);
 	WriteToFile(pngdata, "FFTBARcosine.png");
 	std::cout << "Bar Plot Created" << std::endl;
 	DeleteImage(imageRef->image);
 
-	/* backward Fourier transform, save the result in 'in2' */
+	
+	// backward Fourier transform, save the result in 'in2'
 	printf("\nInverse transform:\n");
 	q = fftw_plan_dft_1d(N, out, in2, FFTW_BACKWARD, FFTW_ESTIMATE);
 	fftw_execute(q);
-	/* normalize */
+	// normalize
 	for (i = 0; i < N; i++) {
 		in2[i][0] *= 1. / N;
 		in2[i][1] *= 1. / N;
@@ -179,11 +143,13 @@ int main()
 
 	fftw_cleanup();
 	return 0;
+	
 }
+*/
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-/*
+// run IMAQ in x86
 int main()
 
 {
@@ -199,7 +165,7 @@ int main()
 
 		{
 			std::cout << "Session is open" << std::endl;
-
+			/*
 			uInt32 top, left, height, width, rowpixels;
 
 			std::cout << "Acquisition Window TOP: ";
@@ -221,6 +187,7 @@ int main()
 			std::cout << "Acquisition Window ROW PIXELS: ";
 			std::cin >> rowpixels;
 			std::cout << "\n";
+			*/
 
 			//imgSnapArea(sessionID, NULL, top, left, height, width, rowpixels);
 			//if (imgSnapArea(sessionID, NULL, top, left, height, width, rowpixels))
@@ -244,4 +211,3 @@ int main()
 
 	}
 }
-*/
